1981aeaf148b21ea3c5825a08d33e4951c94b308
JustAeris <aeris.github@gmail.com>
2024-10-21 09:55:17 +0200

    Changed the settings to use a class. Settings will now be pulled from the EEPROM instead of sitting in the RAM.

--- lib/WWW/Settings.h
+++ lib/WWW/Settings.h
@@ -3,26 +3,42 @@
 
 #include <Arduino.h>
 
-typedef struct Configuration {
-    char LUMIN;
-    int LUMIN_LOW;
-    int LUMIN_HIGH;
-    char TEMP_AIR;
-    char MIN_TEMP_AIR;
-    char MAX_TEMP_AIR;
-    char HYGR;
-    char HYGR_MINT;
-    char HYGR_MAXT;
-    char PRESSURE;
-    int PRESSURE_MIN;
-    int PRESSURE_MAX;
-    char LOG_INTERVAL;
-    int MAX_FILE_SIZE;
-} Configuration;
+// EEPROM addresses
+#define LUMIN_ADDRESS 0
+#define LUMIN_LOW_ADDRESS 1
+#define LUMIN_HIGH_ADDRESS 3
+#define TEMP_AIR_ADDRESS 5
+#define MIN_TEMP_AIR_ADDRESS 6
+#define MAX_TEMP_AIR_ADDRESS 7
+#define HYGR_ADDRESS 8
+#define HYGR_MINT_ADDRESS 9
+#define HYGR_MAXT_ADDRESS 10
+#define PRESSURE_ADDRESS 11
+#define PRESSURE_MIN_ADDRESS 12
+#define PRESSURE_MAX_ADDRESS 14
+#define LOG_INTERVAL_ADDRESS 16
+#define MAX_FILE_SIZE_ADDRESS 17
+
+
+class Configuration {
+    public:
+        static byte LUMIN();
+        static int LUMIN_LOW();
+        static int LUMIN_HIGH();
+        static byte TEMP_AIR();
+        static char MIN_TEMP_AIR();
+        static char MAX_TEMP_AIR();
+        static byte HYGR();
+        static char HYGR_MINT();
+        static char HYGR_MAXT();
+        static byte PRESSURE();
+        static int PRESSURE_MIN();
+        static int PRESSURE_MAX();
+        static byte LOG_INTERVAL();
+        static int MAX_FILE_SIZE();
+};
 
 int tryUpdateSetting(String* input);
-
-void loadSettingsFromEEPROM(Configuration* config);
 
 void resetSettings(Configuration* config);
 
