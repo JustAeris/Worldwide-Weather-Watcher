4d253f9a9619783359a916241900536427f7eebb
JustAeris <aeris.github@gmail.com>
2024-10-22 10:47:12 +0200

    Defined the separator

--- lib/WWW/FS.h
+++ lib/WWW/FS.h
@@ -3,10 +3,11 @@
 #include <Arduino.h>
 #include <Sensors.h>
 
+#define SEPARATOR ','
+
 void saveMeasures(Result* array);
 
 void writeData(String* array, String* fileName, String* directory);
-void readData(String* array, String* fileName, String* directory);
 void archiveData();
 
 #endif //FS_H
